95a6c94df07829f1f67a87c36f32c0bd42e45c56
Aleksey Gurzhiev <AlekseyGur@users.noreply.github.com>
2025-04-28 14:52:25 +0300

    Image stream

---
+++ src/main/java/ru/alexgur/blog/post/interfaces/PostImageService.java
@@ -0,0 +1,13 @@
+package ru.alexgur.blog.post.interfaces;
+
+import java.util.Optional;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import ru.alexgur.blog.post.model.Image;
+
+public interface PostImageService {
+    Optional<Image> load(Long postId);
+
+    void save(Long postId, MultipartFile image);
+}
