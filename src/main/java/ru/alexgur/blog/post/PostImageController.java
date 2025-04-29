fe8891d38407d2e1139b522e71a5bbadfc005267
Aleksey Gurzhiev <AlekseyGur@users.noreply.github.com>
2025-04-29 18:49:46 +0300

    Fix image output stream

--- src/main/java/ru/alexgur/blog/post/PostImageController.java
+++ src/main/java/ru/alexgur/blog/post/PostImageController.java
@@ -1,8 +1,8 @@
 package ru.alexgur.blog.post;
 
-// import java.io.IOException;
-// import java.io.InputStream;
-// import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
@@ -11,8 +11,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.HttpServletBean;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import ru.alexgur.blog.post.interfaces.PostImageService;
 import ru.alexgur.blog.post.model.Image;
@@ -26,7 +26,7 @@ public class PostImageController {
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public void load(@PathVariable Long id, HttpServletBean response) {
+    public void load(@PathVariable Long id, HttpServletResponse response) {
         Optional<Image> imgOpt = postImageService.load(id);
         if (imgOpt.isPresent()) {
             returnImageInStream(response, imgOpt.get());
@@ -34,26 +34,26 @@ public class PostImageController {
         throw new NotFoundException("Файл не найден");
     }
 
-    private void returnImageInStream(HttpServletBean response, Image img) {
-        // try {
-        // InputStream inputStream = img.getInputStream();
+    private void returnImageInStream(HttpServletResponse response, Image img) {
+        try {
+            InputStream inputStream = img.getInputStream();
 
-        // response.setContentType(img.getContentType());
-        // response.setHeader("Content-Length", img.getContentLength().toString());
+            response.setContentType(img.getContentType());
+            response.setHeader("Content-Length", img.getContentLength().toString());
 
-        // OutputStream outputStream = response.getOutputStream();
-        // byte[] buffer = new byte[4096];
-        // int bytesRead;
+            OutputStream outputStream = response.getOutputStream();
+            byte[] buffer = new byte[4096];
+            int bytesRead;
 
-            // while ((bytesRead = inputStream.read(buffer)) != -1) {
-            // outputStream.write(buffer, 0, bytesRead);
-            // }
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
 
-            // inputStream.close();
-            // outputStream.close();
-            // } catch (IOException e) {
-            // response.setStatus(HttpServletBean.);
-            // e.printStackTrace();
-            // }
+            inputStream.close();
+            outputStream.close();
+        } catch (IOException e) {
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            e.printStackTrace();
+        }
     }
 }
