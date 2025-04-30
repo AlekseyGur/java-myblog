a756b813d23d0be710182483156fc49508c18fdd
Aleksey Gurzhiev <AlekseyGur@users.noreply.github.com>
2025-04-30 19:01:06 +0300

    Change image upload dir

--- src/main/java/ru/alexgur/blog/system/config/WebConfig.java
+++ src/main/java/ru/alexgur/blog/system/config/WebConfig.java
@@ -17,6 +17,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/**")
-                .addResourceLocations("classpath:/static/images/");
+                .addResourceLocations("classpath:/upload/images/");
     }
 }
