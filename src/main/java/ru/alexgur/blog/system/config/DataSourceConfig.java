e108a1bd529378551dd9ba41fa3b8705215ea04c
Aleksey Gurzhiev <AlekseyGur@users.noreply.github.com>
2025-05-02 08:20:05 +0300

    Edit datasource

--- src/main/java/ru/alexgur/blog/system/config/DataSourceConfig.java
+++ src/main/java/ru/alexgur/blog/system/config/DataSourceConfig.java
@@ -8,6 +8,8 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
 import javax.sql.DataSource;
@@ -16,11 +18,22 @@ import javax.sql.DataSource;
 @Profile("dev")
 public class DataSourceConfig {
     @Bean
+    @Profile("dev")
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .setName("myblogTestDB")
+                .addScript("schema.sql")
+                .build();
+    }
+
+    @Bean
+    @Profile("prod")
     public DataSource dataSource(
-            @Value("${datasource.url}") String url,
-            @Value("${datasource.username}") String username,
-            @Value("${datasource.password}") String password,
-            @Value("${datasource.driver}") String driverName) {
+            @Value("${datasource.sql.url}") String url,
+            @Value("${datasource.sql.username}") String username,
+            @Value("${datasource.sql.password}") String password,
+            @Value("${datasource.sql.driver}") String driverName) {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName(driverName);
         dataSource.setUrl(url);
@@ -28,8 +41,7 @@ public class DataSourceConfig {
         dataSource.setPassword(password);
 
         ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
-        populator.addScripts(new ClassPathResource("schema.sql"),
-                new ClassPathResource("fake-data.sql"));
+        populator.addScripts(new ClassPathResource("schema.sql"));
         populator.execute(dataSource);
         return dataSource;
     }
